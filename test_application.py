c7138d59e7139778ccf2b504d0d6c41adff76d4e
Bartosz Bryniarski <brylka@jelenia.com>
2025-03-02 11:10:34 +0100

    actions

---
+++ test_application.py
@@ -0,0 +1,20 @@
+import unittest
+from application import application
+
+class FlaskAppTest(unittest.TestCase):
+
+    def setUp(self):
+        self.app = application.test_client()
+        self.app.testing = True
+
+    def test_home_page(self):
+        response = self.app.get('/')
+        date = response.data.decode('utf-8')
+        self.assertIn('Hello, BigData z Pythonem!', date)
+
+    def test_status_hello_page(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
