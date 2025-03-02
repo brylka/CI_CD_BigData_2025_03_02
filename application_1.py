0f216e9bd9ef32124313e0081aee56e4970bf045
Bartosz Bryniarski <brylka@jelenia.com>
2025-03-02 12:52:31 +0100

    actions

--- application_1.py
+++ application_1.py
@@ -1,5 +1,3 @@
-import sqlite3
-
 from flask import Flask
 import os
 
@@ -14,24 +12,6 @@ if ENV == 'dev':
 else:
     pass
     # ustawienia dla wersji produkcyjnej
-
-
-def init_db():
-    conn = sqlite3.connect('tasks.sqlite')
-    c = conn.cursor()
-    c.execute('''
-    CREATE TABLE IF NOT EXISTS tasks (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
-        completed INTEGER DEFAULT 0
-    )
-    ''')
-    conn.commit()
-    conn.close()
-
-init_db()
-
-
 
 
 @application.route('/')
