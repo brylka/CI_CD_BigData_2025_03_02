cb901540485f30537971d70651d2547499a8421c
Bartosz Bryniarski <brylka@jelenia.com>
2025-03-02 12:12:06 +0100

    actions

--- application.py
+++ application.py
@@ -1,12 +1,15 @@
 from flask import Flask
+import os
 
 
 application = Flask(__name__)
 
+#ENV = os.environ.get('FLASK_ENV', 'dev')
 
 @application.route('/')
 def hello_world():
-    return ('<h1>Witaj, BigData z Pythonem!</h1><p>Przykład CI/CD - automatyczne wdrożenie!</p><p>Bartosz Bryniarski</p>')
+    return ('<h1>Witaj, BigData z Pythonem!</h1><p>Przykład CI/CD - automatyczne wdrożenie!</p><p>Bartosz Bryniarski</p>'
+            f'<p>Środowisko: {ENV}</p>')
 
 
 if __name__ == '__main__':
