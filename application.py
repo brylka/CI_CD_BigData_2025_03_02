5ec3cd88ee78b54c00528c0b722235416882cb0a
Bartosz Bryniarski <brylka@jelenia.com>
2025-03-02 12:28:35 +0100

    actions

--- application.py
+++ application.py
@@ -6,13 +6,18 @@ application = Flask(__name__)
 
 ENV = os.environ.get('FLASK_ENV', 'dev')
 
+if ENV == 'dev':
+    pass
+    # ustawienia dla wersji developerskiej
+else:
+    pass
+    # ustawienia dla wersji produkcyjnej
+
+
 @application.route('/')
 def hello_world():
-    if ENV == 'dev':
-        return ('tadam')
-    else:
-        return ('<h1>Witaj, BigData z Pythonem!</h1><p>Przykład CI/CD - automatyczne wdrożenie!</p><p>Bartosz Bryniarski</p>'
-                f'<p>Środowisko: <b>{ENV}</b></p>')
+    return ('<h1>Witaj, BigData z Pythonem!</h1><p>Przykład CI/CD - automatyczne wdrożenie!</p><p>Bartosz Bryniarski</p>'
+            f'<p>Środowisko: <b>{ENV}</b></p>')
 
 
 if __name__ == '__main__':
